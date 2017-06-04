395ef1612b6504f3d3799f8e8b8774aa9abfffc5
Your Name <you@example.com>
2017-06-04 19:43:36 +0000

    changed response type;rewrote all to try:except blocks

--- server.py
+++ server.py
@@ -11,47 +11,48 @@ app = Flask(__name__)
 #default welcome page
 @app.route("/")
 def default():
-    return render_template('index.html',name=request.remote_addr,host=gethostbyname('localhost'))
+    return jsonify({"status":"ok"})
 
 
 #creating storage with inputs
 @app.route("/<filename>/<key>/<value>", methods=['POST'])
 def add_key(filename,key,value):
-    token = request.headers['token']
-    if token_check(token):
-        #creating directory if not exists
-        if not os.path.exists(token):
-            try:
-                os.makedirs(token)
-            except Exception as e:
-                return jsonify({"error":"{}".format(str(e))})
-        #creating file if not exists
-        if not os.path.exists('{}/{}'.format(token,filename)):
-            try:
-                with open('{}/{}'.format(token,filename),'w') as empty_file:
-                    empty_file.write("{}")
-            except Exception as e:
-                return jsonify({"error":"{}".format(str(e))})
-            try:
-                #creating json from inputs
-                with open('{}/{}'.format(token,filename)) as f:
-                    data = json.load(f)
-                    dict_new = {key: value}
-                    data.update(dict_new)
-                with open('{}/{}'.format(token,filename),'w') as outfile:
-                    json.dump(data,outfile,indent=4)
-                return jsonify({"file":"{}".format(filename),"created":"true"})
-            except Exception as e:
-                return jsonify({"error":"{}".format(str(e))})
-    else:
-        return "token is invalid\n"
+    try:
+        token = request.headers['token']
+        if token_check(token):
+            #creating directory if not exists
+            if not os.path.exists(token):
+                try:
+                    os.makedirs(token)
+                except Exception as e:
+                    return jsonify({"error":"{}".format(str(e))})
+            #creating file if not exists
+            if not os.path.exists('{}/{}'.format(token,filename)):
+                try:
+                    with open('{}/{}'.format(token,filename),'w') as empty_file:
+                        empty_file.write("{}")
+                except Exception as e:
+                    return jsonify({"error":"{}".format(str(e))})
+                try:
+                    #creating json from inputs
+                    with open('{}/{}'.format(token,filename)) as f:
+                        data = json.load(f)
+                        dict_new = {key: value}
+                        data.update(dict_new)
+                    with open('{}/{}'.format(token,filename),'w') as outfile:
+                        json.dump(data,outfile,indent=4)
+                    return jsonify({"file":"{}".format(filename),"created":"true"})
+                except Exception as e:
+                    return jsonify({"error":"{}".format(str(e))})
+    except:
+        return jsonify({"error":"token is invalid"})
 
 
 #request whole file
 @app.route("/<filename>")
 def whole_file(filename):
-    token = request.headers['token']
     try:
+        token = request.headers['token']
         with open('{}/{}'.format(token,filename)) as response:
             loaded_response = json.load(response)
         return jsonify(loaded_response)
@@ -62,17 +63,20 @@ def whole_file(filename):
 #request value of certain key
 @app.route("/<filename>/<key>")
 def certain_key(filename,key):
-    token = request.headers['token']
-    with open('{}/{}'.format(token,filename)) as response:
+    try:
+        token = request.headers['token']
+        with open('{}/{}'.format(token,filename)) as response:
             loaded_response = json.load(response)
-    return loaded_response[key] + "\n" 
+        return loaded_response[key] + "\n" 
+    except Exception as e:
+        return jsonify({"error":"{}".format(str(e))})
 
 
 #delete certain key
 @app.route("/<filename>/<key>", methods=['DELETE'])
 def del_key(filename,key):
-    token = request.headers['token']
     try:
+        token = request.headers['token']
         with open('{}/{}'.format(token,filename)) as changing_file:
             changing_data = json.load(changing_file)
             if key in  changing_data:
@@ -89,8 +93,8 @@ def del_key(filename,key):
 #delete whole file
 @app.route("/<filename>", methods=['DELETE'])
 def del_whole_file(filename):
-    token = request.headers['token']
     try:
+        token = request.headers['token']
         os.remove('{}/{}'.format(token,filename))
     except Exception as e:
         return jsonify({"error":"{}".format(str(e))})
@@ -104,7 +108,7 @@ def token_request():
     try:
         with open('tokens_base','a') as f:
             f.write(token+"\n")
-        return render_template('token.html',token=token)
+        return jsonify({"token_request":"ok","token":"{}".format(token)})
     except Exception as e:
         return jsonify({"error":"{}".format(str(e))})
 
